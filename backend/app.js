283c5b44193015850b9e258ca7cf09bee5e46a80
Le <21020322@vnu.edu.vn>
2023-12-09 17:28:51 +0700

    Backend first commit

---
+++ backend/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+const cookieSession = require('cookie-session');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+// const db = require('./models');
+// (async () => {
+//     await db.sequelize.sync();
+// })();
+
+// CORS
+const corsOptions = {
+    origin: "http://localhost:5000",
+};
+
+app.use(cors(corsOptions));
+
+// Session and JSON
+
+
+// Simple route.
+app.get("/", (req, res) => {
+    res.json({message: "Test server 1"});
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
